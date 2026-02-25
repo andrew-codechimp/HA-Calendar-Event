12c25bcacc9a897eb804677b5053a53221dbd814
Andrew Jackson <andrew@codechimp.org>
2026-02-25 08:45:43 +0000

    Fix const

--- custom_components/calendar_event/const.py
+++ custom_components/calendar_event/const.py
@@ -1,8 +1,6 @@
 """Constants for calendar_event."""
 
-import json
 from logging import Logger, getLogger
-from pathlib import Path
 
 from homeassistant.const import Platform
 
@@ -10,14 +8,6 @@ LOGGER: Logger = getLogger(__package__)
 
 MIN_HA_VERSION = "2025.7"
 
-manifestfile = Path(__file__).parent / "manifest.json"
-with open(file=manifestfile, encoding="UTF-8") as json_file:
-    manifest_data = json.load(json_file)
-
-DOMAIN = manifest_data.get("domain")
-NAME = manifest_data.get("name")
-VERSION = manifest_data.get("version")
-ISSUEURL = manifest_data.get("issue_tracker")
 CONFIG_VERSION = 1
 
 PLATFORMS = [Platform.BINARY_SENSOR]
